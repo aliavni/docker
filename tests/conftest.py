876daa78fb0ee5226164766a70b6a9fdf3489234
Ali Cirik <aliavni@gmail.com>
2024-08-11 16:39:39 -0400

    Tests (#15)
    * rename folder
    
    * add tests

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+from utils.dag_config import DagConfig
+
+
+@pytest.fixture(scope="function")
+def dag_config() -> DagConfig:
+    dc = DagConfig(
+        dag_id="test-dag-1",
+        start="2024-07-24",
+        task_ids=[],
+        schedule=None,
+        catchup=False,
+    )
+    return dc
