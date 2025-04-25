65383e8b823b6b255adefa69570158d797c833a5
Ali Cirik <aliavni@gmail.com>
2025-04-24 20:30:57 -0400

    Upgrade Airflow to v3 (#21)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 # syntax=docker/dockerfile:1
-FROM python:3.12.5
+FROM python:3.9.22
 
 COPY requirements.txt /tmp/requirements.txt
 COPY docker/jupyter/requirements.txt /tmp/docker/jupyter/requirements.txt
 COPY docker/airflow/requirements.txt /tmp/docker/airflow/requirements.txt
 
-RUN --mount=type=cache,mode=0755,target=/root/.cache \
-    pip install -r /tmp/requirements.txt
+RUN --mount=type=cache,mode=0755,target=/root/.cache pip install -r /tmp/requirements.txt
 
 WORKDIR /code
 
--- air/dags/airbnb.py
+++ air/dags/airbnb.py
@@ -1,45 +1,54 @@
-"""This DAG demonstrates how to use the `SparkSubmitOperator` to submit a Spark
-job."""
+"""Airflow DAG to fetch and process Airbnb data from a given URL."""
 
-import os
 from datetime import datetime
 
-from airflow.decorators import dag
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from airflow.providers.slack.notifications.slack import send_slack_notification
+import pandas as pd
+import pymongo
+from airflow.providers.mongo.hooks.mongo import MongoHook
+from airflow.sdk import DAG, task
+
+# from airflow.providers.slack.notifications.slack import send_slack_notification
+
+BASE_URL = "https://data.insideairbnb.com/"
 
 
-@dag(
+with DAG(
     dag_id="airbnb",
     params={
-        "url": "https://data.insideairbnb.com/united-states/ma/boston/2024-03-24/data/listings.csv.gz"
+        "url": BASE_URL + "united-states/ma/boston/2025-03-15/data/listings.csv.gz"
     },
     schedule=None,
     start_date=datetime(2024, 5, 20),
     catchup=False,
-    on_success_callback=[
-        send_slack_notification(
-            text=""":large_green_circle: `{{ dag.dag_id }}` dag succeeded at `{{ macros.datetime.utcnow() }}`
-            *processed:* {{ params.url }}
-            """,
-            channel="#airflow-alerts",
-            username="Airflow",
-        )
-    ],
-)
-def airbnb() -> None:
-    """# Airbnb
+    # on_success_callback=[
+    #     send_slack_notification(
+    #         text=""":large_green_circle: `{{ dag.dag_id }}` dag succeeded at `{{ macros.datetime.utcnow() }}`
+    #         *processed:* {{ params.url }}
+    #         """,
+    #         channel="#airflow-alerts",
+    #         username="Airflow",
+    #     )
+    # ],
+) as dag:
 
-    This dag demonstrates how to use `SparkSubmitOperator`
-    """
+    @task.python
+    def get_and_process_data() -> None:
+        """Fetch and process data from the given URL."""
+        df = pd.read_csv(dag.params["url"])
 
+        hook = MongoHook(mongo_conn_id="mongo_default")
+        client: pymongo.MongoClient = hook.get_conn()
+        db: pymongo.database.Database = client.get_database("airbnb")
+        collection: pymongo.collection.Collection = db.get_collection("listings")
 
-airbnb_dag = airbnb()
+        collection.create_index([("id", pymongo.ASCENDING)], unique=True)
 
-spark_task = SparkSubmitOperator(
-    conn_id="spark_master",
-    application=os.path.abspath("dags/airbnb_job.py"),
-    task_id="spark_submit",
-    dag=airbnb_dag,
-    application_args=["--url", "{{ params.url }}"],
-)
+        listings = df.to_dict(orient="records")
+
+        ops = [
+            pymongo.UpdateOne({"id": listing["id"]}, {"$set": listing}, upsert=True)
+            for listing in listings
+        ]
+        collection.bulk_write(ops)
+
+    get_and_process_data()
